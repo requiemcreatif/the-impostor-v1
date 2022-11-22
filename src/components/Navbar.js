73783448457cd4d723fa7303583a429131ea33a3
requiemcreatif <requiemcreatif@gmail.com>
2022-11-22 12:32:40 +0100

    navbar created and stylized

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,61 @@
 import {Link} from "react-router-dom"
+import styled from "styled-components"
 
 import React from 'react'
 
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 2rem 2rem;
+    background: #101B21;
+    box-shadow: 0 0 1rem #ccc;
+    ul {
+        display: flex;
+        list-style: none;
+        li {
+            margin-left: 2rem;
+            //login button
+    }
+}
+`
+const Btn1 = styled.button`
+        padding: 0.5rem 1rem;
+        border: solid 1px #CDF72B;
+        border-radius: 0.5rem;
+        background: transparent;
+        color: #E0F4FF;
+        font-weight: 300;
+        cursor: pointer;
+        &:hover {
+            background: #CDF72B;
+            color: #101B21;
+                
+    }`
+
+const Btn2 = styled.button`
+padding: 0.5rem 1rem;
+border: solid 1px #CDF72B;
+border-radius: 0.5rem;
+background: #CDF72B;
+color: #101B21;
+font-weight: 300;
+cursor: pointer;
+&:hover {
+    background: transparent;
+    
+    color: #E0F4FF;
+}`
+
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <Nav className="navbar">
         <ul>
             <li className="title">Impostr</li>
-            <li><Link to="/Login"><button>Login</button></Link></li>
-            <li><Link to="/Signup"><button>Signup</button></Link></li>
+            <li><Link to="/Login"><Btn1>Login</Btn1></Link></li>
+            <li><Link to="/Signup"><Btn2>Signup</Btn2></Link></li>
         </ul>
-    </nav>
+    </Nav>
   )
 }
 
