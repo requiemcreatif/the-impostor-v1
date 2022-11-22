d5ea2d9de98c74606504b4c6e04e60c4fd013066
requiemcreatif <requiemcreatif@gmail.com>
2022-11-22 11:48:28 +0100

    login card styled using styled component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,33 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 import React from 'react'
+
+const Form = styled.form`
+    margin: 6rem auto;
+    width: 40rem;
+    padding: 2rem;
+    border: 1px solid #ccc;
+    border-radius: 1rem;
+    box-shadow: 0 0 1rem #ccc;
+    h2 {
+        text-align: center;
+        margin-bottom: 2rem;
+    }
+    label {
+        display: block;
+        margin-bottom: 1rem;
+        span {
+            display: block;
+            margin-bottom: 0.5rem;
+        }
+        input {
+            width: 100%;
+            padding: 0.5rem;
+            border: 1px solid #ccc;
+            border-radius: 0.5rem;
+        }
+    }
+`
 
 const Login = () => {
 const [mail, setMail] = useState("")
@@ -7,11 +35,11 @@ const [password, setPassword] = useState("")
 
 function handleSubmit(e) {
     e.preventDefault()
-    //console.log(mail, password);
+    console.log(mail, password);
 }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <h2>Login</h2>
       <label>
         <span>email:</span>
@@ -30,7 +58,7 @@ function handleSubmit(e) {
         />
       </label>
       <button>Login</button>
-    </form>
+    </Form>
   )
 }
 
