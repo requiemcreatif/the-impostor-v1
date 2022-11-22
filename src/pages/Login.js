10c6b981e3e0210fc873c1eb5f9704534d84d6b8
requiemcreatif <requiemcreatif@gmail.com>
2022-11-22 11:31:59 +0100

    Login form created

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,36 @@
+import { useState } from 'react'
 import React from 'react'
 
 const Login = () => {
+const [mail, setMail] = useState("")
+const [password, setPassword] = useState("")
+
+function handleSubmit(e) {
+    e.preventDefault()
+    //console.log(mail, password);
+}
+
   return (
-    <div>You are in the login page</div>
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input 
+        type="email" 
+        onChange={(e) => setMail(e.target.value)}
+        value={mail}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+        type="password" 
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        />
+      </label>
+      <button>Login</button>
+    </form>
   )
 }
 
